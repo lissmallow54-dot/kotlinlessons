ec2d9833d5293082ac4e429f5a92f7ad2b460dce
Elizaveta <lissmallow54@gmail.com>
2025-09-11 21:20:34 +0200

    lesson-1-3 гагарин

---
+++ app/src/main/kotlin/lesson1/1-3-lesson.kt
@@ -0,0 +1,17 @@
+package lesson1
+
+fun main() {
+
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    print(hour)
+    print(":")
+    println(minute)
+}
