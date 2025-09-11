d80991dcdf968f4805085da47599463d13296d2e
Elizaveta <lissmallow54@gmail.com>
2025-09-11 21:02:37 +0200

    lesson-1-1 магазин 72 заказа

---
+++ app/src/main/kotlin/lesson1/1-2-lesson.kt
@@ -0,0 +1,13 @@
+package lesson1
+fun main() {
+    var numberOfEmployees = 2000
+    val numberOfOrders = 72
+    val welcomeMessage = "Спасибо за покупку!"
+    println(numberOfEmployees)
+    println(numberOfOrders)
+    println(welcomeMessage)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+}
