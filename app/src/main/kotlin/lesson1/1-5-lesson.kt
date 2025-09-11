32dfc5f93f4ec32e7c8d5e9359d40baef5260a16
Elizaveta <lissmallow54@gmail.com>
2025-09-11 21:42:06 +0200

    lesson-1-5 5 задание с гагариным

---
+++ app/src/main/kotlin/lesson1/1-5-lesson.kt
@@ -0,0 +1,14 @@
+package lesson1
+fun main() {
+    var seconds: Short = 6480
+    println(seconds)
+    var hour: Byte = 1
+    var minute: Byte = 30
+    var second: Byte = 9
+    print(hour)
+    print(":")
+    print(minute)
+    print(":")
+    println(second)
+
+}
