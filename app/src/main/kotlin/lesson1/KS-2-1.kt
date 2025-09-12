86bf24bc3a6cf960acb54d46a2266748c77c328e
Elizaveta <lissmallow54@gmail.com>
2025-09-12 21:51:52 +0200

    KS-2-1 решение задачи 2 урока 1

---
+++ app/src/main/kotlin/lesson1/KS-2-1.kt
@@ -0,0 +1,15 @@
+package lesson1
+
+fun main () {
+    val numberOfOrders2: Int = 75
+    val thankYouMessage: String = "Спасибо за покупку!"
+    println(numberOfOrders2)
+    println(thankYouMessage)
+
+    var numberOfEmployees: Int = 2000
+    println(numberOfEmployees)
+
+    val numberOfEmployeesAfterResignation: Int = 1999
+    println(numberOfEmployeesAfterResignation)
+
+}
