a464951749b095ead9c679f4b7e82b3efc6c86d2
Elizaveta <lissmallow54@gmail.com>
2025-09-12 22:05:35 +0200

    KS-5-1 решение задачи 5 урока 1

---
+++ app/src/main/kotlin/lesson1/KS-5-1.kt
@@ -0,0 +1,15 @@
+package lesson1
+
+fun main () {
+    var totalSeconds: Int = 6480
+    println(totalSeconds)
+    var hours  = "01"
+    var minutes = "48"
+    var seconds = "00"
+    print(hours)
+    print(":")
+    print(minutes)
+    print(":")
+    println(seconds)
+
+}
