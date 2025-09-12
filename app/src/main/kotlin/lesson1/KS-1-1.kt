b6da808ec9e53c5316f628d51f5799496c91a4b6
Elizaveta <lissmallow54@gmail.com>
2025-09-12 21:39:00 +0200

    KS-1-1 решение задачи 1 урока 1

---
+++ app/src/main/kotlin/lesson1/KS-1-1.kt
@@ -0,0 +1,8 @@
+package lesson1
+
+fun main () {
+    val numberOfOrders: Int = 75
+    val thankYouMessage: String = "Спасибо за покупку!"
+    println(numberOfOrders)
+    println(thankYouMessage)
+}
