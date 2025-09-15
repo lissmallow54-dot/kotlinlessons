e62dd24252696b4adfcdb00c62ddbf410b98b2bc
Elizaveta <lissmallow54@gmail.com>
2025-09-15 22:40:40 +0200

    KS-2-1 второе исправление решения задачи 2 урока 1

--- app/src/main/kotlin/lesson1/KS-2-1.kt
+++ app/src/main/kotlin/lesson1/KS-2-1.kt
@@ -3,11 +3,10 @@ package lesson1
 fun main () {
     val numberOfOrders: Int = 75
     val thankYouMessage: String = "Спасибо за покупку!"
-    var numberOfEmployees: Int = 2000
     println(numberOfOrders)
     println(thankYouMessage)
+    var numberOfEmployees: Int = 2000
     println(numberOfEmployees)
-
     numberOfEmployees = 1999
     println(numberOfEmployees)
 
